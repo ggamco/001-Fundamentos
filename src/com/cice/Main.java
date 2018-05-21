711660d590d1ead6bf7a12d45ceeafb29cb8acb9
Formador <Formador@aula8PovMacForm.local>
2018-05-21 21:56:14 +0200

    Initial commit

---
+++ src/com/cice/Main.java
@@ -0,0 +1,46 @@
+package com.cice;
+
+/**
+ *
+ */
+public class Main {
+
+    private static String nombre = "Gustavo";
+
+    /**
+     * Esto es un comentario para la documentacion del javaDoc
+     * @param args
+     */
+    public static void main(String[] args) {
+        //poner un comentario en una linea
+
+        /*
+        * comentario
+        * en
+        * varias
+        * lineas
+        * */
+
+        byte numero;
+        numero = 127;
+        numero = 124;
+
+        final int CONSTANTE_NO_MUTABLE = 12;
+
+        nombre = "Pedro";
+
+    }
+
+    /**
+     *
+     * @param nombre
+     * @return
+     */
+    public static String saluda(String nombre){
+        int dato = (1 == 2) ? 1 : 2;
+
+        final int CONSTANTE_NO_MUTABLE = 0;
+        return null;
+    }
+
+}
---
+++ src/com/cice/ejercicio1/Main.java
@@ -0,0 +1,11 @@
+package com.cice.ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 2;
+
+        System.out.println("El resultado de la multiplicación es: " + (a*2+6/5));
+
+    }
+}
